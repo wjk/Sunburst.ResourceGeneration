209a77f93ca591bed86ed1acc7c56c3b741f435a
William Kent <wjk@users.noreply.github.com>
2019-02-22 22:01:13 -0500

    Add code from MSBuild to parse resx files

---
+++ src/Sunburst.ResourceGeneration.ResxFormat/MultitargetUtil.cs
@@ -0,0 +1,59 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Resources {
+    using System;
+
+    /// <devdoc>
+    ///     Helper class supporting Multitarget type assembly qualified name resolution for ResX API.
+    ///     Note: this file is compiled into different assemblies (runtime and VSIP assemblies ...)
+    /// </devdoc>
+    internal static class MultitargetUtil
+    {
+        /// <devdoc>
+        ///     This method gets assembly info for the corresponding type. If the delegate
+        ///     is provided it is used to get this information.
+        /// </devdoc>
+        public static string GetAssemblyQualifiedName(Type type, Func<Type, string> typeNameConverter)
+        {
+            string assemblyQualifiedName = null;
+
+            if (type != null)
+            {
+                if (typeNameConverter != null)
+                {
+                    try
+                    {
+                        assemblyQualifiedName = typeNameConverter(type);
+                    }
+                    catch (Exception e)
+                    {
+                        if (IsSecurityOrCriticalException(e))
+                        {
+                            throw;
+                        }
+                    }
+                }
+
+                if (string.IsNullOrEmpty(assemblyQualifiedName))
+                {
+                    assemblyQualifiedName = type.AssemblyQualifiedName;
+                }
+            }
+
+            return assemblyQualifiedName;
+        }
+
+        private static bool IsSecurityOrCriticalException(Exception ex)
+        {
+            return ex is NullReferenceException
+                    || ex is StackOverflowException
+                    || ex is OutOfMemoryException
+                    || ex is System.Threading.ThreadAbortException
+                    || ex is IndexOutOfRangeException
+                    || ex is AccessViolationException
+                    || ex is System.Security.SecurityException;
+        }
+    }
+}
