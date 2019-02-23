a0828e916494a91dd4b9ecef1368067490282535
William Kent <wjk@users.noreply.github.com>
2019-02-23 13:58:04 -0500

    Add ResGenTest.Resx test project

---
+++ test/ResGenTest.Resx/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ResGenTest.Resx
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Resources.HelloString);
+            System.Globalization.CultureInfo.CurrentUICulture = new System.Globalization.CultureInfo("de-DE");
+            Console.WriteLine(Resources.HelloString);
+        }
+    }
+}
